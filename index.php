8500008d2240e5c9d88b586ce4cf5121cb6cb196
owl-vg-1 <owl-vg-1@rambler.ru>
2019-08-27 19:02:05 +0300

    Добавлен css

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/style.css">
     <title>Гостевая книга</title>
 </head>
 <body>

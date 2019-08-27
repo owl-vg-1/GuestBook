fe258d8c063f26ac676c76c45f310cc5fa8bbaf5
owl-vg-1 <owl-vg-1@rambler.ru>
2019-08-27 20:55:41 +0300

    обновление

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Гостевая книга</title>
 </head>
 <body>
     <h1>Старт гостевой книги111<h1>
 
-    <h2> <?php include_once('new.php');?></h2>
+    <h2> <?php include_once('../new.php');?></h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et temporibus alias unde dolorum eaque ullam, debitis amet asperiores porro voluptatum officia nemo eum sapiente cumque enim deserunt rem numquam. Fuga!</p>
     <ol>
         <li>123</li>

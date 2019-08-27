6d202db5e498a3bfeda3181a0e93a6ce9a85308e
owl-vg-1 <owl-vg-1@rambler.ru>
2019-08-27 18:35:11 +0300

    Начало нового проекта

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Гостевая книга</title>
+</head>
+<body>
+    <h1>Старт гостевой книги<h1>
+</body>
+</html>
